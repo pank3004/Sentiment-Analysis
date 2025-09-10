6e418239651e6efc53e74b0cf3e747358cc1a76c
pank3004 <pankajkumawatew3004@gmail.com>
2025-09-10 17:30:03 +0530

    test 11

--- tests/test_flask_app.py
+++ tests/test_flask_app.py
@@ -1,7 +1,23 @@
 import unittest
 from flask_app.app import app
+import os
+import mlflow
 
 class FlaskAppTests(unittest.TestCase):
+    # Set up DagsHub credentials for MLflow tracking
+    dagshub_token = os.getenv("CAPSTONE_TEST")
+    if not dagshub_token:
+        raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
+
+    os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+    os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+    dagshub_url = "https://dagshub.com"
+    repo_owner = "pank3004"
+    repo_name = "Sentiment-Analysis"
+
+    # Set up MLflow tracking URI
+    mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
     @classmethod
     def setUpClass(cls):
@@ -19,6 +35,7 @@ class FlaskAppTests(unittest.TestCase):
             b'Positive' in response.data or b'Negative' in response.data,
             "Response should contain either 'Positive' or 'Negative'"
         )
+
 
 if __name__ == '__main__':
     unittest.main()
