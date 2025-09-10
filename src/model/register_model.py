15fb0abeb7de03186c48adf2517774f71f2db9fb
pank3004 <pankajkumawatew3004@gmail.com>
2025-09-10 16:22:26 +0530

    test 6

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -13,27 +13,27 @@ warnings.filterwarnings("ignore")
 
 # Below code block is for production use
 # -------------------------------------------------------------------------------------
-# Set up DagsHub credentials for MLflow tracking
-# dagshub_token = os.getenv("CAPSTONE_TEST")
-# if not dagshub_token:
-#     raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
+#Set up DagsHub credentials for MLflow tracking
+dagshub_token = os.getenv("CAPSTONE_TEST")
+if not dagshub_token:
+    raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
 
-# os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
-# os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
-# dagshub_url = "https://dagshub.com"
-# repo_owner = "vikashdas770"
-# repo_name = "YT-Capstone-Project"
-# # Set up MLflow tracking URI
-# mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+dagshub_url = "https://dagshub.com"
+repo_owner = "pank3004"
+repo_name = "Sentiment-Analysis"
+# Set up MLflow tracking URI
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 # -------------------------------------------------------------------------------------
 
 
 # Below code block is for local use
 # -------------------------------------------------------------------------------------
-logging.info('set mlflow with dagshub')
-mlflow.set_tracking_uri('https://dagshub.com/pank3004/Sentiment-Analysis.mlflow')
-dagshub.init(repo_owner='pank3004', repo_name='Sentiment-Analysis', mlflow=True)
+# logging.info('set mlflow with dagshub')
+# mlflow.set_tracking_uri('https://dagshub.com/pank3004/Sentiment-Analysis.mlflow')
+# dagshub.init(repo_owner='pank3004', repo_name='Sentiment-Analysis', mlflow=True)
 # -------------------------------------------------------------------------------------
 
 
